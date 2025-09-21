9d062943e4e6a3c77842ef00997eaa955778fe4d
jaeminjo732 <jaeminjo732@gmail.com>
2025-09-21 16:00:47 +0900

    feat(#4) :: 커스텀 익셥선 수정

--- src/main/java/baro/baro/domain/auth/exception/EmailException.java
+++ src/main/java/baro/baro/domain/auth/exception/EmailException.java
@@ -2,10 +2,22 @@ package baro.baro.domain.auth.exception;
 
 import baro.baro.domain.common.exception.BusinessException;
 import baro.baro.domain.common.exception.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class EmailException extends BusinessException {
 
-    public EmailException(EmailErrorCode errorCode) {
-        super(ErrorCode.INTERNAL_ERROR);
+    private final EmailErrorCode emailErrorCode;
+
+    public EmailException(EmailErrorCode emailErrorCode) {
+        super(convertToErrorCode(emailErrorCode));
+        this.emailErrorCode = emailErrorCode;
+    }
+
+    private static ErrorCode convertToErrorCode(EmailErrorCode emailErrorCode) {
+        return switch (emailErrorCode.getStatus()) {
+            case 400 -> ErrorCode.VALIDATION_ERROR;
+            default -> ErrorCode.INTERNAL_ERROR;
+        };
     }
 }
--- src/main/java/baro/baro/domain/auth/exception/PhoneVerificationException.java
+++ src/main/java/baro/baro/domain/auth/exception/PhoneVerificationException.java
@@ -2,10 +2,22 @@ package baro.baro.domain.auth.exception;
 
 import baro.baro.domain.common.exception.BusinessException;
 import baro.baro.domain.common.exception.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class PhoneVerificationException extends BusinessException {
 
-    public PhoneVerificationException(PhoneVerificationErrorCode errorCode) {
-        super(ErrorCode.VALIDATION_ERROR);
+    private final PhoneVerificationErrorCode phoneErrorCode;
+
+    public PhoneVerificationException(PhoneVerificationErrorCode phoneErrorCode) {
+        super(convertToErrorCode(phoneErrorCode));
+        this.phoneErrorCode = phoneErrorCode;
+    }
+
+    private static ErrorCode convertToErrorCode(PhoneVerificationErrorCode phoneErrorCode) {
+        return switch (phoneErrorCode.getStatus()) {
+            case 500 -> ErrorCode.INTERNAL_ERROR;
+            default -> ErrorCode.VALIDATION_ERROR;
+        };
     }
 }
