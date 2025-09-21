6c407721aac2a9050c67840b2600ea4394ffe912
youngw0130 <162306763+youngw0130@users.noreply.github.com>
2025-09-22 00:24:29 +0900

    feat(#2): 인증 요청 DTO 추가

---
+++ src/main/java/baro/baro/domain/auth/dto/req/LoginRequest.java
@@ -0,0 +1,13 @@
+package baro.baro.domain.auth.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
