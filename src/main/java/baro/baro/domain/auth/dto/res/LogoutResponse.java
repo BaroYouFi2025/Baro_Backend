0a185231a2a07cfd357f7e0caf85c96fe449e632
youngw0130 <162306763+youngw0130@users.noreply.github.com>
2025-09-22 00:25:28 +0900

    feat(#2): 인증 응답 DTO 추가

---
+++ src/main/java/baro/baro/domain/auth/dto/res/LogoutResponse.java
@@ -0,0 +1,10 @@
+package baro.baro.domain.auth.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LogoutResponse {
+    private String message;
+}
