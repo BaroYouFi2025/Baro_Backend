06b7fca7d8aa11344223ade01eea2f286ae1829d
jaeminjo732 <jaeminjo732@gmail.com>
2025-09-21 15:40:31 +0900

    feat(#4) :: 패키지 변경

--- src/main/java/baro/baro/domain/user/service/UserService.java
+++ src/main/java/baro/baro/domain/user/service/UserService.java
@@ -1,8 +1,8 @@
 package baro.baro.domain.user.service;
 
+import baro.baro.domain.auth.dto.res.AuthTokensResponse;
 import baro.baro.domain.user.dto.req.SignupRequest;
 import baro.baro.domain.user.entity.User;
-import baro.baro.domain.user.dto.res.AuthTokensResponse;
 
 public interface UserService {
     User createUser(String uid, String rawPassword, String phone, String name, String birthDateIso);
