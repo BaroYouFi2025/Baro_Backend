03f1d0bb2c51b98c26474c461e5ab74c3f1e5132
youngw0130 <162306763+youngw0130@users.noreply.github.com>
2025-10-16 17:14:33 +0900

    refacter(#2): HttpServletResponse response 추가

--- src/main/java/baro/baro/domain/auth/service/AuthService.java
+++ src/main/java/baro/baro/domain/auth/service/AuthService.java
@@ -1,16 +1,16 @@
 package baro.baro.domain.auth.service;
 
-
 import baro.baro.domain.auth.dto.req.LoginRequest;
 import baro.baro.domain.auth.dto.req.LogoutRequest;
 import baro.baro.domain.auth.dto.req.RefreshRequest;
 import baro.baro.domain.auth.dto.res.AuthTokensResponse;
 import baro.baro.domain.auth.dto.res.LogoutResponse;
 import baro.baro.domain.auth.dto.res.RefreshResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
-    AuthTokensResponse login(LoginRequest request);
-    LogoutResponse logout(LogoutRequest request);
+    AuthTokensResponse login(LoginRequest request, HttpServletResponse response);
+    LogoutResponse logout(LogoutRequest request, HttpServletResponse response);
     RefreshResponse refresh(RefreshRequest request);
 }
 
