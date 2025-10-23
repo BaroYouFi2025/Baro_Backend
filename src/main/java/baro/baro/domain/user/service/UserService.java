de018b6058335448fee6dd3c060a35554451d10a
youngw0130 <162306763+youngw0130@users.noreply.github.com>
2025-10-23 14:32:46 +0900

    feat(#19): UserService에 getProfile 메서드 정의

--- src/main/java/baro/baro/domain/user/service/UserService.java
+++ src/main/java/baro/baro/domain/user/service/UserService.java
@@ -2,10 +2,12 @@ package baro.baro.domain.user.service;
 
 import baro.baro.domain.auth.dto.res.AuthTokensResponse;
 import baro.baro.domain.user.dto.req.SignupRequest;
+import baro.baro.domain.user.dto.res.UserProfileResponse;
 import baro.baro.domain.user.entity.User;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
     User createUser(String uid, String rawPassword, String phone, String name, String birthDateIso);
     AuthTokensResponse signup(SignupRequest request, HttpServletResponse response);
+    UserProfileResponse getProfile();
 }
