af4806092411287b865b514ecac402395746e67c
youngw0130 <162306763+youngw0130@users.noreply.github.com>
2025-10-23 14:44:14 +0900

    feat(#23): UpdateMissingPersonRequest DTO 추가

---
+++ src/main/java/baro/baro/domain/missingperson/dto/req/UpdateMissingPersonRequest.java
@@ -0,0 +1,19 @@
+package baro.baro.domain.missingperson.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateMissingPersonRequest {
+    private String name;
+    private Integer age;
+    private String gender;
+    private String description;
+    private String location;
+    private String lastSeenDate;
+}
