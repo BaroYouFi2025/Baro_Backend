deee5185c785c03e82b76fd01961aa29e1eb6c6f
jaeminjo732 <jaeminjo732@gmail.com>
2025-10-27 17:39:12 +0900

    refactor(#44) :: 문서에 맞게 수정

--- src/main/java/baro/baro/domain/missingperson/dto/req/UpdateMissingPersonRequest.java
+++ src/main/java/baro/baro/domain/missingperson/dto/req/UpdateMissingPersonRequest.java
@@ -1,23 +1,15 @@
 package baro.baro.domain.missingperson.dto.req;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateMissingPersonRequest {
     private String name;
-    
-    @JsonProperty("birth_date")
+
     private String birthDate;
-    
-    @JsonProperty("photo_url")
+
     private String photoUrl;
-    
-    @JsonProperty("missing_date")
+
     private String missingDate;
     
     private Integer height;
@@ -25,17 +17,13 @@ public class UpdateMissingPersonRequest {
     private Integer weight;
     
     private String body;
-    
-    @JsonProperty("bodyEtc")
+
     private String bodyEtc;
-    
-    @JsonProperty("clothesTop")
+
     private String clothesTop;
-    
-    @JsonProperty("clothesBottom")
+
     private String clothesBottom;
-    
-    @JsonProperty("clothesEtc")
+
     private String clothesEtc;
     
     private String location;
