5218e54a0005f645cf8ce8c43b4c67c1b10f3384
jaeminjo732 <jaeminjo732@gmail.com>
2025-10-28 00:38:35 +0900

    fix(#44) :: 쿼리 수정

--- src/main/java/baro/baro/domain/missingperson/repository/MissingPersonRepository.java
+++ src/main/java/baro/baro/domain/missingperson/repository/MissingPersonRepository.java
@@ -21,13 +21,24 @@ public interface MissingPersonRepository extends JpaRepository<MissingPerson, Lo
                        "WHERE mc.case_status = 'OPEN'",
            nativeQuery = true)
     Page<MissingPerson> findAllOpenCases(Pageable pageable);
-    
-    @Query(value = "SELECT DISTINCT mp.* FROM youfi.missing_persons mp " +
-                   "INNER JOIN youfi.missing_cases mc ON mp.id = mc.missing_person_id " +
-                   "WHERE mc.case_status = 'OPEN' " +
-                   "AND ST_DWithin(mp.location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :radius) " +
-                   "ORDER BY ST_Distance(mp.location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326))",
-           nativeQuery = true)
+
+    @Query(value = """
+SELECT DISTINCT
+  mp.*,
+  ST_Distance(
+    mp.location::geography,
+    ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography
+  ) AS distance
+FROM youfi.missing_persons mp
+JOIN youfi.missing_cases mc ON mp.id = mc.missing_person_id
+WHERE mc.case_status = 'OPEN'
+  AND ST_DWithin(
+    mp.location::geography,
+    ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography,
+    :radius
+  )
+ORDER BY distance
+""", nativeQuery = true)
     List<MissingPerson> findNearbyMissingPersons(@Param("latitude") Double latitude, 
                                                 @Param("longitude") Double longitude, 
                                                 @Param("radius") Integer radius);
