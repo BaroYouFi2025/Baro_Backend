89a9c14d10b15b131d438a4b14633475c6d58d02
jaeminjo732 <jaeminjo732@gmail.com>
2025-11-06 21:16:40 +0900

    fix(#53) :: solve merge issue

--- src/main/java/baro/baro/domain/missingperson/repository/MissingCaseRepository.java
+++ src/main/java/baro/baro/domain/missingperson/repository/MissingCaseRepository.java
@@ -1,6 +1,8 @@
 package baro.baro.domain.missingperson.repository;
 
+import baro.baro.domain.missingperson.entity.CaseStatusType;
 import baro.baro.domain.missingperson.entity.MissingCase;
+import baro.baro.domain.missingperson.entity.MissingPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,6 @@ public interface MissingCaseRepository extends JpaRepository<MissingCase, Long> 
 
     @Query("SELECT COUNT(m) FROM MissingCase m WHERE m.reportedBy = :userId and m.caseStatus = 'OPEN'")
     long countByReportedById(Long userId);
+
+    Optional<MissingCase> findByMissingPersonAndCaseStatus(MissingPerson missingPerson, CaseStatusType caseStatusType);
 }
