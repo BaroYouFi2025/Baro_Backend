07d29db0940be2253a65d3a76eb46bd0f7e4c587
jaeminjo732 <jaeminjo732@gmail.com>
2025-11-13 17:26:12 +0900

    fix(#47) : 불필요한 Service 삭제

--- src/main/java/baro/baro/domain/notification/controller/NotificationController.java
+++ src/main/java/baro/baro/domain/notification/controller/NotificationController.java
@@ -29,7 +29,6 @@ import java.util.List;
 public class NotificationController {
 
     private final NotificationService notificationService;
-    private final PushNotificationService pushNotificationService;
 
     @Operation(summary = "내 알림 목록 조회", description = "현재 로그인한 사용자의 모든 알림을 조회합니다.")
     @ApiResponses({
