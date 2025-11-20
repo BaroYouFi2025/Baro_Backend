cc1bd20903037ae62e5a8204e4c7efa61411226b
jaeminjo732 <jaeminjo732@gmail.com>
2025-11-21 08:35:46 +0900

    feat(#80) :: ExternalApiException 추가
    - ExternalApiException.java: 외부 API 오류를 비즈니스 예외로 표현할 수 있는 래퍼를 도입해 호출부에서 구체적인 에러 코드를 전달

---
+++ src/main/java/baro/baro/domain/common/exception/ExternalApiException.java
@@ -0,0 +1,12 @@
+package baro.baro.domain.common.exception;
+
+public class ExternalApiException extends BusinessException {
+
+    public ExternalApiException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public ExternalApiException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode, cause);
+    }
+}
