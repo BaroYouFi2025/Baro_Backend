069707bf9e8d90d9468aa66b867b695e16cfdd3a
jaeminjo732 <jaeminjo732@gmail.com>
2025-11-21 08:44:21 +0900

    feat(#80) :: 기기 API 권한 보호
    - DeviceController.java: ADMIN/USER 롤만 접근하도록 @PreAuthorize를 추가

--- src/main/java/baro/baro/domain/device/controller/DeviceController.java
+++ src/main/java/baro/baro/domain/device/controller/DeviceController.java
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/devices")
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN','USER')")
 public class DeviceController {
 
     private final DeviceService deviceService;
