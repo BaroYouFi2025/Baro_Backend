5348a420e19b3d66322c4ab14a452b3aed35f296
jaeminjo732 <jaeminjo732@gmail.com>
2025-11-25 15:40:51 +0900

    fix(#84) :: logout시 fcm 토큰 무력화

--- src/main/java/baro/baro/domain/device/service/DeviceService.java
+++ src/main/java/baro/baro/domain/device/service/DeviceService.java
@@ -5,9 +5,11 @@ import baro.baro.domain.device.dto.req.FcmTokenUpdateRequest;
 import baro.baro.domain.device.dto.req.GpsUpdateRequest;
 import baro.baro.domain.device.dto.res.DeviceResponse;
 import baro.baro.domain.device.dto.res.GpsUpdateResponse;
+import baro.baro.domain.device.dto.event.LogoutSuccessEvent;
 
 public interface DeviceService {
     DeviceResponse registerDevice(DeviceRegisterRequest request);
     GpsUpdateResponse updateGps(GpsUpdateRequest request);
     void updateFcmToken(String uid, FcmTokenUpdateRequest request);
+    void handleLogout(LogoutSuccessEvent event);
 }
